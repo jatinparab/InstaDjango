9125f3962c16f9bead3eba32a1782735307d027f
Jatin Parab <sulphurgfx@gmail.com>
2018-04-05 12:44:04 +0530

    Profile fix

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((6tlw$+dac=7y^2rx(i6&_9o+rrh!lq4m(i@1h3=8ls*k&f^x'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost','127.0.0.1'
+]
 
 
 # Application definition
